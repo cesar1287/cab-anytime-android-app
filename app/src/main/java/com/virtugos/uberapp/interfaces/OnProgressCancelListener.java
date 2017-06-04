38044f0ed5cc63387c81f8c067f22a033c29577a
César Rodrigues <rasec1287@hotmail.com>
2017-06-04 17:16:44 -0300

    initial commit

---
+++ app/src/main/java/com/virtugos/uberapp/interfaces/OnProgressCancelListener.java
@@ -0,0 +1,9 @@
+package com.virtugos.uberapp.interfaces;
+
+/**
+ * @author Elluminati elluminati.in
+ */
+public interface OnProgressCancelListener {
+	public void onProgressCancel();
+
+}
---
+++ app/src/main/java/com/virtugos/uberapp/parse/AsyncTaskCompleteListener.java
@@ -0,0 +1,8 @@
+package com.virtugos.uberapp.parse;
+
+/**
+ * @author Elluminati elluminati.in
+ */
+public interface AsyncTaskCompleteListener {
+	void onTaskCompleted(String response, int serviceCode);
+}
